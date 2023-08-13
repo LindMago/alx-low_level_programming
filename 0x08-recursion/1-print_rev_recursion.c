13c64af5dd61df44b2bf34d59fe3bd8bad362ed7
Lindmago <lindmago@gmail.com>
2023-08-13 11:00:07 -0700

    Task 0

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
-/*
- * _print_rev_recursion - print string in reverse
+/**
+ * _print_rev_recursion - Print string in reverse.
  * @s: input string
  *
  * Return: void
  */
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
